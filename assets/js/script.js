408c6dd6fe2b9007a42b80521f0df48eca97a077
lucasrenandns <lucasrenandns96@gmail.com>
2022-11-24 16:33:41 -0300

    Update website

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 const header = document.querySelector("header")
 const navlist = document.querySelector(".nav-list")
 const menuIcon = document.querySelector("#menu-icon")
+const scrollArrowContainer = document.querySelector(".scroll-arrow")
+const scrollArrow = document.querySelector("#up-arrow")
 
-window.addEventListener("scroll", () => {
+document.addEventListener("scroll", () => {
     header.classList.toggle("sticky", window.scrollY > 0)
 })
 
@@ -13,7 +15,25 @@ menuIcon.addEventListener("click", (event) => {
     menuIcon.classList.toggle("bx-x")
 })
 
-window.addEventListener("scroll", () => {
+document.addEventListener("scroll", () => {
     navlist.classList.remove("active")
     menuIcon.classList.remove("bx-x")
 })
+
+scrollArrow.addEventListener("click", () => {
+    offSet()
+})
+
+const offSet = () => {
+    window.scrollTo(0, 0)
+}
+
+const offSetTop = () => {
+    if(window.scrollY > 1000) {
+        scrollArrowContainer.style.display = "flex"
+    }
+    else {
+        scrollArrowContainer.style.display = "none"
+    }
+}
+document.addEventListener("scroll", offSetTop)
