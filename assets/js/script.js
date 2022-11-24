693837f1072bbfc291eddcc326df624bff09f340
lucasrenandns <lucasrenandns96@gmail.com>
2022-11-24 14:12:50 -0300

    Update website

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
 const header = document.querySelector("header")
+const navlist = document.querySelector(".nav-list")
+const menuIcon = document.querySelector("#menu-icon")
 
 window.addEventListener("scroll", () => {
     header.classList.toggle("sticky", window.scrollY > 0)
+})
+
+menuIcon.addEventListener("click", (event) => {
+    event.preventDefault()
+
+    navlist.classList.toggle("active")
+    menuIcon.classList.toggle("bx-x")
+})
+
+window.addEventListener("scroll", () => {
+    navlist.classList.remove("active")
+    menuIcon.classList.remove("bx-x")
 })
